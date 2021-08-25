035ae9b1066990cee05d391e7b99582b0601fa4d
Pedro Bufulin <pedro.almeida@ufu.br>
2021-08-25 08:00:21 -0300

    refatoração do BN

--- VerySimple/src/migrations/1_deploy_contracts.js
+++ VerySimple/src/migrations/1_deploy_contracts.js
@@ -1,8 +1,8 @@
 const verySimple = artifacts.require("VerySimple");
-
+const BN = web3.utils.BN;
 module.exports = function (deployer) {
-  var a = new web3.utils.BN(2);
-  var b = new web3.utils.BN(255);
-  MAX_INT = a.pow(b).sub( new web3.utils.BN(1));
+  var a = new BN(2);
+  var b = new BN(255);
+  var MAX_INT = a.pow(b).sub(new BN(1));
   deployer.deploy(verySimple, MAX_INT);
 };
