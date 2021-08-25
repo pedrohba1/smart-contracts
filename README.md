8304d6e1c188ec36dad710e545eebd2a0dc004e5
Pedro Bufulin <pedro.almeida@ufu.br>
2021-08-25 10:09:59 -0300

    atualização do README, mais um caso de teste no very simple fix

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ truffle exec src/scripts/interage.js
 ### motivações desse projeto
 
 Esse projeto consiste em testar o underflow e o overflow na linguagem solidity na versão 0.5.10.
+
+Um dos jeitos foi usando o contrato `UnsignedSafeMath.sol` disponibilizado pelo OpenZeppelin. Esse contrato estabelece um valor máximo para operações aritméticas e outras validações que evitam a ocorrência de overflows e underflows.
+
+O outro jeito que é bem mais simples do que usar esse contrato é usar uma versão do Solidity maior que 0.8.0. Dessa versão em diante o Solidity já checa e evita a ocorrência de overflows e underflows por padrão. 
--- VerySimple/src/test/VerySimpleFix.js
+++ VerySimple/src/test/VerySimpleFix.js
@@ -9,9 +9,29 @@ contract("VerySimpleFix", accounts =>{
         console.log(balance.toString(16));
         console.log('bits ocupados antes da tentativa de overflow:', balance.bitLength());
         try {
-        const res = await contract.inc();
+        await contract.inc();
         } catch (e) {
             console.log(e.message);
         }
+    })
+
+    it('deveria  ser capaz de decrementar e incrementar, nessa ordem, normalmente', async () => {
+        const contract = await instance.deployed();
+        var balance = await contract.getBalance.call();
+        console.log('valor do balance em hexadecimal antes do overflow:');
+        console.log(balance.toString(16));
+        console.log('bits ocupados antes da tentativa de overflow:', balance.bitLength());
+        try {
+        await contract.dec();
+        await contract.dec();
+
+        } catch (e) {
+            console.log(e.message);
+        }
+
+        var balance = await contract.getBalance.call();
+        console.log('valor do balance em hexadecimal antes do overflow:');
+        console.log(balance.toString(16));
+        console.log('bits ocupados antes da tentativa de overflow:', balance.bitLength());
     })
 });
