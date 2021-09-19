001febbfde323449bf85190cbf75f2c9d94682d1
pedrohba1 <pedro.almeida@ufu.br>
2021-09-19 19:54:07 -0300

    testes de deploy e uso da funçao do leilao

--- LMU/contracts/LMU.sol
+++ LMU/contracts/LMU.sol
@@ -1,10 +1,12 @@
 pragma solidity >=0.4.25 <0.6.0;
+import "hardhat/console.sol";
 
 contract LMU {
     enum EstadosLeilao {
         LancesEstado,
         FinalizadoEstado
     }
+
     EstadosLeilao meuEstado;
 
     struct LancesValor {
@@ -23,19 +25,21 @@ contract LMU {
     uint256 premio;
 
     constructor(uint256 tempo) public payable {
+        console.log("enviante da mensagem %s", msg.sender);
         blocklimit = block.number + tempo;
         meuEstado = EstadosLeilao.LancesEstado;
         vencedorval = 0;
         dono = msg.sender;
         pagou = false;
         premio = msg.value;
+        console.log("valor enviado para o contrato %s", msg.value);
     }
 
     function fazLance() public payable {
         verificaFim();
         require(meuEstado == EstadosLeilao.LancesEstado, "Fim dos lances...");
+        console.log("valor que chegou no contrato", msg.value);
         LancesValor storage l = lances[msg.value];
-
         if (l.valor > 0) {
             l.ehUnico = false;
             l.usuarios.push(msg.sender);
--- LMU/test/deploy.js
+++ LMU/test/deploy.js
@@ -1,18 +1,22 @@
+const { BigNumber } = require("@ethersproject/bignumber");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Deploy do LMU", function () {
   it("should deploy contract", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+    
+
     const LMU = await ethers.getContractFactory("LMU");
-    const [owner] = await ethers.getSigners();
+    
+    const lmu = await LMU.deploy(5, {value: 10});
+    await lmu.deployed();
 
-    const lmu = await LMU.deploy(5);
-    const deployed = await lmu.deployed();
+    await lmu.connect(addr1).fazLance({value: 40})
 
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-      console.log(account.address);
-    }
+    // console.log(BigNumber.from(await owner.getBalance()).toString());
+    // for (const account of accounts) {
+    //   console.log(account.address);
+    // }
   });
 });
