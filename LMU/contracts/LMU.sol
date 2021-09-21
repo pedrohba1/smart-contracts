13c2a9a6d7a9ad0045cc93568c6010dfe07fbe84
Pedro Bufulin <pedro.almeida@ufu.br>
2021-09-21 19:01:59 -0300

    funçao paga vencedor

--- LMU/contracts/LMU.sol
+++ LMU/contracts/LMU.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.4.25 <0.6.0;
+
 import "hardhat/console.sol";
 
 contract LMU {
@@ -12,7 +13,7 @@ contract LMU {
     struct LancesValor {
         uint256 valor;
         bool ehUnico;
-        address[] usuarios;
+        address payable[] usuarios;
     }
 
     mapping(uint256 => LancesValor) lances;
@@ -25,17 +26,15 @@ contract LMU {
     uint256 premio;
 
     constructor(uint256 tempo) public payable {
-        console.log("enviante da mensagem %s", msg.sender);
         blocklimit = block.number + tempo;
         meuEstado = EstadosLeilao.LancesEstado;
         vencedorval = 0;
         dono = msg.sender;
         pagou = false;
         premio = msg.value;
-        console.log("valor enviado para o contrato %s", msg.value);
     }
 
-    function pegMLU() public view returns (uint256) {
+    function pegaMLU() public view returns (uint256) {
         uint256 v = 0;
         LancesValor storage l = lances[v];
         while (v < 10 ^ 18) {
@@ -52,7 +51,6 @@ contract LMU {
     function fazLance() public payable {
         verificaFim();
         require(meuEstado == EstadosLeilao.LancesEstado, "Fim dos lances...");
-        console.log("valor que chegou no contrato", msg.value);
         LancesValor storage l = lances[msg.value];
         if (l.valor > 0) {
             l.ehUnico = false;
@@ -61,6 +59,19 @@ contract LMU {
             l.ehUnico = true;
             l.valor = msg.value;
             l.usuarios.push(msg.sender);
+        }
+    }
+
+
+    function pegaVencedor() public {
+        require(meuEstado == EstadosLeilao.FinalizadoEstado, "O leilão ainda nãoa acabou");
+        require(pagou ==false, "Premio já foi pago");
+        uint v = pegaMLU();
+        LancesValor storage l  = lances[v];
+        l.usuarios[0].send(premio);
+        pagou = true;
+        if(!l.usuarios[0].send(premio)){
+            pagou = false;
         }
     }
 
