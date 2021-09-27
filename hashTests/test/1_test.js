f31299f510b2e34ce09b70f2b0db8bafb1ea53a0
Pedro Bufulin <pedro.almeida@ufu.br>
2021-09-27 06:38:08 -0300

    consegui gerar duas hashes iguais acrescentando o padding

--- hashTests/test/1_test.js
+++ hashTests/test/1_test.js
@@ -6,18 +6,23 @@ function byteCount(s) {
     return encodeURI(s).split(/%..|./).length - 1;
 }
 
+function padUntil32Bytes(string) {
+    string = string.padEnd(32,'0');
+    return string;
+}
+
 contract("Testes do SimpleCommit", async accounts => {
     it("O deploy do contrato deveria ser feito", async () => {
         const instance = await HashContract.deployed();
     });
     it("A hash retornada deveria ser igual a criada fora do contrato", async () => {
         const instance = await HashContract.deployed();
-        segredo = 'A';
-
-        const result = await instance.SimpleHash.call(segredo);
+        secret = web3.utils.toHex('A');
+        secret = padUntil32Bytes(secret)
+        const result = await instance.SimpleHash.call(secret);
         console.log('function return',result);
 
-        hash = SHA256('A');
+        hash = SHA256(secret);
         console.log('hash outside the contract', hash.toString(CryptoJS.enc.Hex));
     });
 });
