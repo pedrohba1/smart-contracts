e32a7468d4c2c2a189a2c1cbed606ee2d1f236b9
Pedro Bufulin <pedro.almeida@ufu.br>
2021-09-30 20:25:55 -0300

    deploy com tempo

--- ParImpar/migrations/1_initial_migration.js
+++ ParImpar/migrations/1_initial_migration.js
@@ -2,7 +2,10 @@ const SimpleCommit = artifacts.require("SimpleCommit");
 const ParImpar = artifacts.require("ParImpar");
 
 module.exports = function (deployer, network, accounts) {
+  startTime = Date.now();
+  endTime = startTime + 10000;
+
   deployer.deploy(SimpleCommit);
   deployer.link(SimpleCommit, ParImpar);
-  deployer.deploy(ParImpar, accounts[0], accounts[1]);
+  deployer.deploy(ParImpar, accounts[0], accounts[1], startTime, endTime);
 };
