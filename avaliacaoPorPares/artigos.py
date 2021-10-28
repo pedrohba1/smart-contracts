0da6357ebfbc59c8e0a01ca650f81efbc424a0ea
Pedro Bufulin <pedro.almeida@ufu.br>
2021-10-28 03:00:34 +0000

    avaliação por pares

---
+++ avaliacaoPorPares/artigos.py
@@ -0,0 +1,30 @@
+import hashlib
+
+ 
+
+artigos = list()
+g = 999
+
+# valor no segredo do commit
+b = 137 # O MEU SEGREDO
+a = 18822 # O SEGREDO QUE O PROFESSOR PASSOU
+p = 27143
+N = 14 # são 14 artigos (por enquanto)
+
+ 
+T = 5 # cada aluno lê e avalia 5 artigos 
+print(a,b)
+
+shared_secret_key = g**(a*b) %p
+h = str(shared_secret_key).encode();
+while len(artigos) < T:
+    h = hashlib.sha256(h).digest()
+    # lembrando que como é o modulo, vai retornar de 0 até n-1. 
+    # então o primeiro artigo é 0, o segundo é 1, o terceiro é 2...
+    i = int.from_bytes(h, "big")% N;
+    if i not in artigos:
+        artigos.append(i)
+
+ 
+
+print(artigos)
---
+++ avaliacaoPorPares/calcula.py
@@ -0,0 +1,6 @@
+g =999
+b = 137 # O MEU SEGREDO
+p = 27143
+
+
+print((g **b) % p)
